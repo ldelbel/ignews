425fa1479bf1b30bce12627b797d8f2a1212502c
Lucas Delbel <lucdelbel@gmail.com>
2021-04-05 11:03:04 -0300

    All the way up to getServerSideProps using Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,57 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    value: number;
+  };
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏️ Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.value} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IcsSlKWOdgt4GQk3VisAJAP", {
+    expand: ["product"], // expand é usado para pegar informações do produto, caso queiramos mostrá-los na página
+  });
+
+  const product = {
+    priceId: price.id,
+    value: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
