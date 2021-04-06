c3dfc9b47f97d7346e18bd4e755c8a582650f83e
Lucas Delbel <lucdelbel@gmail.com>
2021-04-06 11:29:16 -0300

    Add saveSubscription function to save the sub on database for later handling

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,19 +43,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (relevantEvents.has(type)) {
       try {
-        switch(type){
-        case 'checkout.session.completed':
-          break;
-        default:
-          throw new Error('Unhaldled event.')
+        switch (type) {
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session; // typed checkoutSession to know the data inside checkout object, instad of generic stripe object
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error("Unhaldled event.");
         }
       } catch (err) {
-      return res.json({ error: 'Webhook handler failed.'})
-    }
+        return res.json({ error: "Webhook handler failed." });
+      }
 
-    res.json({ received: true });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method not allowed");
+      res.json({ received: true });
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method not allowed");
+    }
   }
 };
